d4c44c726e4bfd14150665dcd3f84ec7990d37c3
Paridhi <agarwalparidhi10@gmail.com>
2023-07-19 05:08:24 +0800

    WelcomePage.test.js, FamilyHomePage.test.js, NavBar.test.js, ErrorAlert.test.js tests passed

---
+++ src/test/LayoutTests/ErrorAlert.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import BasicAlerts from "../../components/Layout/ErrorAlert";
+import "@testing-library/jest-dom/extend-expect"
+import { toBeInTheDocument } from "@testing-library/jest-dom/matchers";
+
+describe("BasicAlerts", () => {
+  test("renders BasicAlerts component with error message", () => {
+    const errorMessage = "This is an error message";
+
+    const {getByText}= render(<BasicAlerts errorText={errorMessage} />);
+
+    // Check if the error message is rendered correctly
+    expect(screen.getByText(errorMessage)).toBeInTheDocument();
+  });
+});
