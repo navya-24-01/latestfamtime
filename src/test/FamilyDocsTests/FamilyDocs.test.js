d89bcbeb0f5b020e2c4e63ffc371bd370dc8aa26
Paridhi <agarwalparidhi10@gmail.com>
2023-07-19 03:38:30 +0800

    FamilyDocs testing completed

--- src/test/FamilyDocsTests/FamilyDocs.test.js
+++ src/test/FamilyDocsTests/FamilyDocs.test.js
@@ -2,30 +2,40 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter, useLocation } from "react-router-dom";
 import FamilyDocs from "../../components/FamilyDocs/FamilyDocs";
+import { AuthContext } from "../../contexts/AuthContext";
+
+jest.mock("../../components/Layout/NavBar", () => {
+  return function MockNavBar() {
+    return <div>Mocked NavBar</div>; // Provide a mock implementation for the NavBar component
+  };
+});
 
 jest.mock("react-router-dom", () => ({
-    useLocation: jest.fn(),
-  }));
-  
-  const mockLocation = {
-    state: {
-      familyid: "test-family-id",
-    },
-  };
+  ...jest.requireActual("react-router-dom"),
+  useLocation: jest.fn(),
+}));
 
+const currentUser = { uid: "jefnbhkehfn" };
 
 describe("FamilyDocs", () => {
-  test("renders FamilyDocs component", () => {
+  test("renders FamilyDocs component without errors", () => {
+    const mockLocation = {
+      state: {
+        familyid: "test-family-id",
+      },
+    };
+
     useLocation.mockReturnValue(mockLocation);
 
     render(
-      <MemoryRouter>
-        <FamilyDocs />
-      </MemoryRouter>
+      <AuthContext.Provider value={{ currentUser }}>
+        <MemoryRouter>
+          <FamilyDocs />
+        </MemoryRouter>
+      </AuthContext.Provider>
     );
-
-    expect(container).toBeDefined();
-    // Add your assertions here to test the component's behavior
   });
 });
+
+
 
