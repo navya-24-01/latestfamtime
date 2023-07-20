b00b25a0ac8734eb257b25aa4993418f84ff3b7f
Paridhi <agarwalparidhi10@gmail.com>
2023-07-21 04:01:51 +0800

    isolated FamilyDocsTests

---
+++ src/test/AuthTests/ProfilePage.test.js
@@ -0,0 +1,43 @@
+import React from "react"; 
+import { render, fireEvent, waitFor } from "@testing-library/react"; 
+import { FireBaseContext } from "../../contexts/FireBaseFunctions"; 
+import ProfilePage from "../../components/Auth/ProfilePage"; 
+import { MemoryRouter, Route, useNavigate } from "react-router-dom"; 
+import "@testing-library/jest-dom/extend-expect"; 
+ 
+ 
+const message = "Creating your profile"; 
+const setUser = jest.fn(); 
+const checkUserExists = jest.fn().mockResolvedValue(false); 
+ 
+jest.mock("react-router-dom", () => { 
+  const originalModule = jest.requireActual("react-router-dom"); 
+  return { 
+    ...originalModule, 
+    useNavigate: () => jest.fn(), 
+  }; 
+}); 
+ 
+describe("ProfilePage", () => { 
+      
+  it("renders without errors when a new user enters", async () => { 
+    render( 
+      <FireBaseContext.Provider 
+        value={{ 
+          message, 
+          setUser, 
+          checkUserExists: jest.fn().mockResolvedValue(false), 
+        }} 
+      > 
+        <MemoryRouter> 
+          <ProfilePage /> 
+        </MemoryRouter> 
+      </FireBaseContext.Provider> 
+    ); 
+  }); 
+});
+
+
+
+
+
