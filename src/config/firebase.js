9f895c2660f4a0c9125f78a68ff6fe73168a248f
navya-24-01 <134744331+navya-24-01@users.noreply.github.com>
2023-07-21 22:46:58 +0800

    update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { initializeFirestore } from "firebase/firestore";
+import { initializeFirestore, getFirestore} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,8 +23,10 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 //export const db = getFirestore(app);
 
-export const db = initializeFirestore(app, {
+/*export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true, 
-})
+})*/
+
+export const db = getFirestore(app)
 
 export default app;
